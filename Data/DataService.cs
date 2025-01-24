318c76011aaf10d716a3d4777f8f0d0bdb7ad1ee
Craig Roberts <craig.roberts11@outlook.com>
2025-01-25 06:11:01 +1030

    Added tailwindcss as local package

--- Data/DataService.cs
+++ Data/DataService.cs
@@ -27,4 +27,26 @@ public class DataService
 
         return room.Houses;
     }
+
+    public async Task UpdateRoomScores(Room newRoom)
+    {
+        var room = await _context.Rooms
+            .Where(r => r.RoomNumber == newRoom.RoomNumber)
+            .Include(r => r.Houses)
+            .ThenInclude(h => h.Events)
+            .FirstOrDefaultAsync();
+        
+        if (room is null)
+        {
+            throw new Exception("Room not found");
+        }
+
+        foreach (var house in newRoom.Houses)
+        {
+            room.Houses = newRoom.Houses;
+        }
+        
+        _context.Rooms.Update(room);
+        await _context.SaveChangesAsync();
+    }
 }
